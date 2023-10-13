310b6cba3b92c9f89336d3903dee5c2665f082db
Dayane Sallet <dayanesallet@hotmail.com>
2023-10-13 15:03:34 -0300

    includes a password hash and verify for existing email in database

--- src/http/controller/register.ts
+++ src/http/controller/register.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
+import { hash } from 'bcryptjs'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -10,11 +11,24 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   })
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const userWithEmail = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if (userWithEmail) {
+    throw new Error('Email already registered.')
+    return reply.status(409).send()
+  }
+
+  const password_hash = await hash(password, 6)
+
   await prisma.user.create({
       data: {
           name,
           email,
-          password_hash: password
+          password_hash
       }
   })
 

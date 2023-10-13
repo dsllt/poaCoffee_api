5c9de788289dea6a0b8f9db0846108ded2671baf
Dayane Sallet <dayanesallet@hotmail.com>
2023-10-13 12:07:15 -0300

    creates  in prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 export const app = fastify();
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: 'ds',
+        email: 'test'
+    }
+})
+
+
+// docker run --name api-poaCoffee-pg -e POSTGRESQL_USERNAME=docker -e POSTGRESQL_PASSWORD=docker -e POSTGRESQL_DATABASE=poacoffee -p 5432:5432 bitnami/postgresql

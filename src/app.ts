0753c9af86d00e043127d07ad5f8319a92f15c3c
Dayane Sallet <dayanesallet@hotmail.com>
2023-10-13 14:56:51 -0300

    creates a controller for user registration

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,10 @@
 // docker run --name api-poaCoffee-pg -e POSTGRESQL_USERNAME=docker -e POSTGRESQL_PASSWORD=docker -e POSTGRESQL_DATABASE=poacoffee -p 5432:5432 bitnami/postgresql
 import fastify from "fastify";
-import { prisma } from "./lib/prisma";
-import { z } from "zod";
+import { appRoutes } from "./http/routes";
 
 export const app = fastify();
 
-app.post('/users', async (request, reply) => {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6)
-    })
-    const { name, email, password } = registerBodySchema.parse(request.body)
-
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: password
-        }
-    })
-
-    return reply.status(201).send()
-})
-
+app.register(appRoutes)
 
 
 
